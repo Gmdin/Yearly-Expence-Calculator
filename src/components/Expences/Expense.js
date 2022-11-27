c999152edab74ae1cb6fae08f4c7c4829060df43
unknown <gmdin716@gmail.com>
2022-11-27 22:25:34 +0500

    Add conditions to check expence year wise

--- src/components/Expences/Expense.js
+++ src/components/Expences/Expense.js
@@ -8,16 +8,23 @@ const Expense=(props)=>{
       const  onSelectChangeYear=(change)=>{
         setFilterYear(change);
       }
+      const filterYearExpence=props.expense.filter((expence)=>{
+        return expence.dat.getFullYear().toString()===filterYear;
+      });
+      let expenceContnet=<p>No Record find</p>;
+      if(filterYearExpence.length>0){
+        expenceContnet=filterYearExpence.map((expense)=>(
+          <ExpenceItem 
+          key={expense.id}
+           title={expense.title} 
+           price={expense.price} 
+           date={expense.dat}/>
+        ));
+      }
     return(
         <Cards className="expense">
             <ExpenceFilter selected={filterYear} onSelectChangeYear={onSelectChangeYear}/>
-            {props.expense.map((expense)=>(
-              <ExpenceItem 
-              key={expense.id}
-               title={expense.title} 
-               price={expense.price} 
-               date={expense.dat}/>
-            ))};
+           {expenceContnet};
             </Cards>
     );
 }
